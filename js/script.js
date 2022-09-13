834cef5ca7cf29d385c6dbfc917e508d17140228
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-13 21:39:12 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ Descrizione:
 Partendo dal markup in allegato, rifare lo slider ma questa volta usando Vue. 
 Potete tenere sott'occhio la versione fatta in plain JS come riferimento.
 
+
 Bonus:
 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 
 Consigli del giorno:
 - regola d'oro: riciclare ovunque possibile! 
